d1968535ac26dabf2533dc80afff97289e49c7d2
Goryounov <goryounow@gmail.com>
2021-08-13 16:04:07 +0300

    Revert "Initializing the project"
    This reverts commit bddd5ec5cda147d95666c041223544abbfb906b2.

--- routes/OrderRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const orderController = require("../controllers/OrderController.js");
-const router = express.Router();
-
-router.get("/order", orderController.order);
-router.post("/finish-order", orderController.finishOrder);
-
-module.exports = router;
